404a8b4dd2e5b82d35f86c44fa320a3404661117
Matubber Mohammad Eashin <mohammadeashin.matubber@aalto.fi>
2023-10-29 18:40:08 +0100

    task model init

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Task(models.Model):
+    project_ref = models.CharField(max_length=50,blank=False, null=False)
+    description = models.TextField()
+    assigned_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    priority = models.CharField(
+        max_length=1, 
+        choices=[("h", "High"),("m", "Medium")],
+        default="h"
+    )
+
+    
